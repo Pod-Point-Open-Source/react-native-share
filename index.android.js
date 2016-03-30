7b49d870aba8a8af1cb3992e50ede1d5cc7279d1
Juan Serra <juanvisp1@gmail.com>
2016-03-30 13:19:03 +0100

    Set default callback for Android share component

--- index.android.js
+++ index.android.js
@@ -1,2 +1,14 @@
 var { NativeModules } = require('react-native');
-module.exports = NativeModules.RNShare;
+var NativeRNShare = NativeModules.RNShare;
+
+/**
+ * High-level docs for the RNShare Android API can be written here.
+ */
+
+var RNShare = {
+    open: function(options, callback = function() {}) {
+        NativeRNShare.open(options, callback);
+    }
+};
+
+module.exports = RNShare;
